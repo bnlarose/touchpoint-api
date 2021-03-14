85f8d258e8d83043dfa2ce16bd1af7ea9d43007f
Berhane-Hiwet La Rose <berhane.larose@gmail.com>
2021-03-14 00:31:14 -0400

    Implement dotenv in server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // Import ApolloServer constructor
 const { ApolloServer } = require('apollo-server')
 
+// Import environment variables
+require('dotenv').config()
+
 // Import typeDefs and resolvers
 const typeDefs = require('./src/typeDefs')
 const resolvers = require('./src/resolvers')
